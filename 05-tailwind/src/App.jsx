7563b16d37716c09f3e1ec234c04808b39ea9d21
AgniAditya <agniaditya05@gmail.com>
2026-05-03 13:15:26 +0530

    tailwind css in vite bundler

---
+++ 05-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-red-500 flex justify-center h-screen w-full'>
+      <h1>App</h1>
+    </div>
+  )
+}
+
+export default App
